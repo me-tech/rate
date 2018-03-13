fa5f5f4ac17a4bfb2722226d354e098857178e0f
Victor Siu <victor.siu.528@gmail.com>
2018-03-13 19:15:48 +0800

    register done

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ var mysql = require('mysql');
 var app = express();
 app.use(bodyParser.json());
 
+var hash = require("password-hash");
+var validator = require('validator');
+
 var db = require('./conn.js'); var transporter = require('./sendEmail.js');
 //var db = require('./connLocal.js'); var transporter = require('./sendEmailLocal.js');
 
@@ -21,15 +24,13 @@ app.use(fileUpload());
 app.get('/api/browse/prof', function(req, res) {
     console.log(req.path);
     console.log(req.body);
-
-    res.status(200);
     res.type('json');
 
     var sql = 'SELECT User.Uname, User.Type, User.SchoolShort, User.Department, University.SchoolName, AVG(Rating.RateScore) AS RateScore, User.Email FROM User, University, Major, Rating WHERE User.SchoolShort = University.SchoolShort AND User.Mshort = Major.Mshort AND User.Type = "Professor" AND Rating.ProMail = User.Email GROUP BY Rating.ProMail';
     db.query(sql, function(err, rows) {
         if (err) {
             console.log(err);
-            dberror();
+            res.status(500).end(errorCode(500, "Database Error"));
         }else{
             var objs = [];
             for (var i = 0;i < rows.length; i++) {
@@ -37,7 +38,7 @@ app.get('/api/browse/prof', function(req, res) {
                 objs.push({Professor: rows[i].Uname, Type: rows[i].Type, SchoolName: rows[i].SchoolName, SchoolShort: rows[i].SchoolShort, Department: rows[i].Department, RateScore: rows[i].RateScore});
             }
             var result = {result: objs};
-            res.end(JSON.stringify(result));
+            res.status(200).end(JSON.stringify(result));
         }
     });
 
@@ -47,22 +48,21 @@ app.get('/api/browse/prof', function(req, res) {
 app.get('/api/browse/course', function(req, res) {
     console.log(req.path);
     console.log(req.body);
-
-    res.status(200);
     res.type('json');
 
     var sql = 'SELECT ID, Cname, Code, Major FROM Course';
     db.query(sql, function(err, rows) {
         if (err) {
             console.log(err);
-            dberror();
+            res.status(500).end(errorCode(500, "Database Error"));
         }else{
             var objs = [];
             for (var i = 0;i < rows.length; i++) {
                 objs.push({ID: rows[i].ID, Cname: rows[i].Cname, Code: rows[i].Code, Major: rows[i].Major});
             }
             var result = {result: objs};
-            res.end(JSON.stringify(result));
+            
+            res.status(200).end(JSON.stringify(result));
         }
     });
 
@@ -74,38 +74,51 @@ app.post('/api/register', function(req, res) {
     res.type('json');
 
     var type = req.body.type;
-    var uname = req.body.uname;
+    console.log(type);
+    var uname = mysql.escape(req.body.uname);
     var email = req.body.email;
-    var password = req.body.password;
-    var major = req.body.programmeName;
     var school = 'OUHK';
+    var password = req.body.password;
+    var major = req.body.major;
+    var department = req.body.department;
 
-    if(!email || !password || !uname || !major || !type){
-        res.end(JSON.stringify({error: "Please fill in all fields."}));
+    if(!email || !password || !uname || !type){
+        res.status(400).end(JSON.stringify({error: "Please fill in all fields."}));
+    }else if(!major && !department){
+        res.status(400).end(JSON.stringify({error: "Please fill in all fields."}));
     }else{
-        var input_array = [];
-        input_array.push(uname,email,type,password, school, major);
-        var sql = 'INSERT INTO User(Uname, Email, Type, Password, SchoolShort, Mshort) VALUES(?, ?, ?, ?, ?)';
-        sql = mysql.format(sql, input_array);
-        db.query(sql, function(err, rows) {
-            if (err) {
-                console.log(err);
-                res.end(errorCode(500,"Database connection error"));
-            }else{
-                try{
-                    if(rows[0].Uname == null || !rows[0].Uname){
-                        res.end(errorCode(403, "Wrong email or password"));
-                    }else{
-                        console.log(rows[0].Uname);
-                        res.end(JSON.stringify({Uname: rows[0].Uname, Type: rows[0].Type, SchoolName: rows[0].SchoolName, Mname: rows[0].Mname, SchoolShort: rows[0].SchoolShort, Mshort: rows[0].Mshort}));
-                    }
-                }catch(e){
-                    if(e instanceof TypeError){
-                        res.end(errorCode(403, "Wrong email or password"));
-                    }
+        if(!validator.isEmail(email) || !domainCheck(email)){
+            res.status(400).end(JSON.stringify({error: "Please provide a proper email address."}));
+        }else if(type==='Student' ||type==='Professor'){
+            var input_array = [];
+            password = hash.generate(password);
+            console.log('here');
+
+            try{
+                if(type=='Student'){
+                input_array.push(uname,email,type,password,school,major);
+                var sql = 'INSERT INTO User(Uname, Email, Type, Password, SchoolShort, Mshort) VALUES(?, ?, ?, ?, ?, ?)';
                 }
+
+                if(type=='Professor'){
+                    input_array.push(uname,email,type,password,school,department);
+                    var sql = 'INSERT INTO User(Uname, Email, Type, Password, SchoolShort, Department) VALUES(?, ?, ?, ?, ?, ?)';
+                }
+            }finally{
+                console.log(sql);
+                sql = mysql.format(sql, input_array);
+                db.query(sql, function(err, rows) {
+                    if (err) {
+                        console.log(err);
+                        res.status(500).end(errorCode(500,"Database connection error"));
+                    }else{
+                        res.status(200).end(JSON.stringify({result: "Sucessfully registered."}))
+                    }
+                });
             }
-        });
+        }else{
+            console.log('type wrong');
+        }
     }
 
 });
@@ -114,13 +127,12 @@ app.post('/api/login', function(req, res) {
     console.log(req.path);
     console.log(req.body);
     res.type('json');
-    res.status(200);
 
     var email = req.body.email;
     var password = req.body.password;
 
     if(!email || !password){
-        res.end(JSON.stringify({error: "email or password missing"}));
+        res.status(400).end(JSON.stringify({error: "email or password missing"}));
     }else{
         var input_array = [];
         input_array.push(email);
@@ -130,18 +142,18 @@ app.post('/api/login', function(req, res) {
         db.query(sql, function(err, rows) {
             if (err) {
                 console.log(err);
-                res.end(errorCode(500,"Database connection error"));
+                res.status(500).end(errorCode(500,"Database connection error"));
             }else{
                 try{
                     if(rows[0].Uname == null || !rows[0].Uname){
-                        res.end(errorCode(403, "Wrong email or password"));
+                        res.status(403).end(errorCode(403, "Wrong email or password"));
                     }else{
                         console.log(rows[0].Uname);
-                        res.end(JSON.stringify({Uname: rows[0].Uname, Type: rows[0].Type, SchoolName: rows[0].SchoolName, Mname: rows[0].Mname, SchoolShort: rows[0].SchoolShort, Mshort: rows[0].Mshort}));
+                        res.status(200).end(JSON.stringify({Uname: rows[0].Uname, Type: rows[0].Type, SchoolName: rows[0].SchoolName, Mname: rows[0].Mname, SchoolShort: rows[0].SchoolShort, Mshort: rows[0].Mshort}));
                     }
                 }catch(e){
                     if(e instanceof TypeError){
-                        res.end(errorCode(403, "Wrong email or password"));
+                        res.status(403).end(errorCode(403, "Wrong email or password"));
                     }
                 }
             }
@@ -158,7 +170,7 @@ app.post('/api/forget', function(req, res) {
     var email = req.body.email;
 
     if(!email){
-        res.end(JSON.stringify({error: "email missing"}));
+        res.status(400).end(errorCode(400,"Email is missing"));
     }else{
         var input_array = [];
         input_array.push(email);
@@ -206,8 +218,7 @@ app.get('/api/search/user/:key/:value', function(req,res){
     res.type('json');
 
     if(req.params.key == "Student"){
-        res.status(204);
-        res.end(errorCode(204,"No Content for Student"));
+        res.status(204).end(errorCode(204,"No Content for Student"));
     }
 
     if(req.params.key == "Professor"){
@@ -291,10 +302,6 @@ function errorCode(status, message){
     return JSON.stringify({"status": status, "message": message});
 }
 
-function dberror(){
-    return res.end(errorCode(500, "Database Error"));
-}
-
 function sendEmail(to, subject, html) {
     transporter.sendMail({
       from: 'rate@metech.fighter.hk',
@@ -308,6 +315,18 @@ function sendEmail(to, subject, html) {
         console.log('Success: ' + info);
       }
     });
+}
+
+function domainCheck(email) {
+    var whiteList = ['ouhk.edu.hk'];
+
+    var splitArray = email.split('@');
+
+    if(whiteList.indexOf(splitArray[1]) >= 0){
+        return true;
+    }
+
+    return false;
 }
 
 app.listen(process.env.PORT || 8099);
