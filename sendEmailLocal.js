7ceab24c80b23ed99da61cc543408775996399b0
Victor Siu <victor.siu.528@gmail.com>
2018-03-13 19:19:07 +0800

    Delete sendEmailLocal.js

--- sendEmailLocal.js
+++
@@ -1,6 +0,0 @@
-var nodemailer = require('nodemailer');
-var sparkPostTransport = require('nodemailer-sparkpost-transport');
-var transporter = nodemailer.createTransport(sparkPostTransport({sparkPostApiKey: '80952a63a56a518089c9b5aa0342a240ac221c6d'}));
-
-
-module.exports = transporter;
