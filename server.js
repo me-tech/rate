f3895e9c3138ad778a8dd07e8b6f5db6e1abb34d
Victor Siu <victor.siu.528@gmail.com>
2018-03-14 11:41:53 +0800

    change env

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(bodyParser.json());
 var hash = require("password-hash");
 var validator = require('validator');
 
-//var db = require('./conn.js'); var transporter = require('./sendEmail.js');
-var db = require('./connLocal.js'); var transporter = require('./sendEmailLocal.js');
+var db = require('./conn.js'); var transporter = require('./sendEmail.js');
+//var db = require('./connLocal.js'); var transporter = require('./sendEmailLocal.js');
 
 app.use(fileUpload());
 
