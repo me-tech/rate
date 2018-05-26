99ff972d4791cb20fc7922ecaf745616f3cb413f
Victor Siu <victor.siu.528@gmail.com>
2018-05-26 19:22:38 +0800

    fix connection error

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/api/browse/:key', function(req, res) {
 
     pool.getConnection(function(err, connection) {
         connection.query(sql, function(err, rows) {
-            connection.release();
+            connection.destroy();
             if (err) {
                 debug(err);
                 res.status(500).end(errorCode(500, "Database Error"));
